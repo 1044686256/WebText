a54de5b52e9a5c7ca13e973bd27b22c911fcc6e1
Lenovo <Lenovo@DESKTOP-4DVILVE>
2017-08-10 08:38:28 +0800

    添加项目文件。

---
+++ Form1.Designer.cs
@@ -0,0 +1,386 @@
+﻿namespace WebText
+{
+    partial class MainFrm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.webBrowser2 = new System.Windows.Forms.WebBrowser();
+            this.webBrowser3 = new System.Windows.Forms.WebBrowser();
+            this.webBrowser4 = new System.Windows.Forms.WebBrowser();
+            this.webBrowser5 = new System.Windows.Forms.WebBrowser();
+            this.webBrowser6 = new System.Windows.Forms.WebBrowser();
+            this.webBrowser7 = new System.Windows.Forms.WebBrowser();
+            this.webBrowser8 = new System.Windows.Forms.WebBrowser();
+            this.webBrowser9 = new System.Windows.Forms.WebBrowser();
+            this.webBrowser10 = new System.Windows.Forms.WebBrowser();
+            this.webBrowser11 = new System.Windows.Forms.WebBrowser();
+            this.webBrowser12 = new System.Windows.Forms.WebBrowser();
+            this.webBrowser13 = new System.Windows.Forms.WebBrowser();
+            this.webBrowser14 = new System.Windows.Forms.WebBrowser();
+            this.lab = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(0, 0);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(250, 250);
+            this.webBrowser1.TabIndex = 0;
+            this.webBrowser1.Url = new System.Uri("", System.UriKind.Relative);
+            // 
+            // webBrowser2
+            // 
+            this.webBrowser2.Location = new System.Drawing.Point(256, 0);
+            this.webBrowser2.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser2.Name = "webBrowser2";
+            this.webBrowser2.Size = new System.Drawing.Size(250, 250);
+            this.webBrowser2.TabIndex = 0;
+            this.webBrowser2.Url = new System.Uri("", System.UriKind.Relative);
+            // 
+            // webBrowser3
+            // 
+            this.webBrowser3.Location = new System.Drawing.Point(512, 0);
+            this.webBrowser3.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser3.Name = "webBrowser3";
+            this.webBrowser3.Size = new System.Drawing.Size(250, 250);
+            this.webBrowser3.TabIndex = 0;
+            this.webBrowser3.Url = new System.Uri("", System.UriKind.Relative);
+            // 
+            // webBrowser4
+            // 
+            this.webBrowser4.Location = new System.Drawing.Point(768, 0);
+            this.webBrowser4.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser4.Name = "webBrowser4";
+            this.webBrowser4.Size = new System.Drawing.Size(250, 250);
+            this.webBrowser4.TabIndex = 0;
+            this.webBrowser4.Url = new System.Uri("", System.UriKind.Relative);
+            // 
+            // webBrowser5
+            // 
+            this.webBrowser5.Location = new System.Drawing.Point(0, 274);
+            this.webBrowser5.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser5.Name = "webBrowser5";
+            this.webBrowser5.Size = new System.Drawing.Size(250, 250);
+            this.webBrowser5.TabIndex = 0;
+            this.webBrowser5.Url = new System.Uri("", System.UriKind.Relative);
+            // 
+            // webBrowser6
+            // 
+            this.webBrowser6.Location = new System.Drawing.Point(256, 274);
+            this.webBrowser6.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser6.Name = "webBrowser6";
+            this.webBrowser6.Size = new System.Drawing.Size(250, 250);
+            this.webBrowser6.TabIndex = 0;
+            this.webBrowser6.Url = new System.Uri("http://www.51yuezi.cn/index.html", System.UriKind.Absolute);
+            // 
+            // webBrowser7
+            // 
+            this.webBrowser7.Location = new System.Drawing.Point(512, 274);
+            this.webBrowser7.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser7.Name = "webBrowser7";
+            this.webBrowser7.Size = new System.Drawing.Size(250, 250);
+            this.webBrowser7.TabIndex = 0;
+            this.webBrowser7.Url = new System.Uri("http://www.4ecnc.com/", System.UriKind.Absolute);
+            // 
+            // webBrowser8
+            // 
+            this.webBrowser8.Location = new System.Drawing.Point(768, 274);
+            this.webBrowser8.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser8.Name = "webBrowser8";
+            this.webBrowser8.Size = new System.Drawing.Size(250, 250);
+            this.webBrowser8.TabIndex = 0;
+            this.webBrowser8.Url = new System.Uri("http://bbm.fanglinktec.com/", System.UriKind.Absolute);
+            // 
+            // webBrowser9
+            // 
+            this.webBrowser9.Location = new System.Drawing.Point(0, 547);
+            this.webBrowser9.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser9.Name = "webBrowser9";
+            this.webBrowser9.Size = new System.Drawing.Size(250, 250);
+            this.webBrowser9.TabIndex = 0;
+            this.webBrowser9.Url = new System.Uri("http://www.hanqingshijue.cn/", System.UriKind.Absolute);
+            // 
+            // webBrowser10
+            // 
+            this.webBrowser10.Location = new System.Drawing.Point(256, 547);
+            this.webBrowser10.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser10.Name = "webBrowser10";
+            this.webBrowser10.Size = new System.Drawing.Size(250, 250);
+            this.webBrowser10.TabIndex = 0;
+            this.webBrowser10.Url = new System.Uri("http://www.hgqlwh.com/", System.UriKind.Absolute);
+            // 
+            // webBrowser11
+            // 
+            this.webBrowser11.Location = new System.Drawing.Point(512, 547);
+            this.webBrowser11.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser11.Name = "webBrowser11";
+            this.webBrowser11.Size = new System.Drawing.Size(250, 250);
+            this.webBrowser11.TabIndex = 0;
+            this.webBrowser11.Url = new System.Uri("http://bbm.fanglinktec.com/", System.UriKind.Absolute);
+            // 
+            // webBrowser12
+            // 
+            this.webBrowser12.Location = new System.Drawing.Point(768, 547);
+            this.webBrowser12.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser12.Name = "webBrowser12";
+            this.webBrowser12.Size = new System.Drawing.Size(250, 250);
+            this.webBrowser12.TabIndex = 0;
+            this.webBrowser12.Url = new System.Uri("http://www.sdjysx.net/", System.UriKind.Absolute);
+            // 
+            // webBrowser13
+            // 
+            this.webBrowser13.Location = new System.Drawing.Point(1024, 0);
+            this.webBrowser13.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser13.Name = "webBrowser13";
+            this.webBrowser13.Size = new System.Drawing.Size(250, 250);
+            this.webBrowser13.TabIndex = 0;
+            this.webBrowser13.Url = new System.Uri("", System.UriKind.Relative);
+            // 
+            // webBrowser14
+            // 
+            this.webBrowser14.Location = new System.Drawing.Point(1024, 274);
+            this.webBrowser14.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser14.Name = "webBrowser14";
+            this.webBrowser14.Size = new System.Drawing.Size(250, 250);
+            this.webBrowser14.TabIndex = 0;
+            this.webBrowser14.Url = new System.Uri("http://mx.fanglinktec.com/", System.UriKind.Absolute);
+            // 
+            // lab
+            // 
+            this.lab.AutoSize = true;
+            this.lab.Location = new System.Drawing.Point(12, 9);
+            this.lab.Name = "lab";
+            this.lab.Size = new System.Drawing.Size(89, 12);
+            this.lab.TabIndex = 1;
+            this.lab.Text = "123.232.114.60";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(276, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(522, 8);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(781, 7);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 12);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(1038, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 12);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "label4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(14, 292);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(41, 12);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "label5";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(278, 292);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(41, 12);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "label6";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(521, 292);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(41, 12);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "label7";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(783, 292);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(41, 12);
+            this.label8.TabIndex = 9;
+            this.label8.Text = "label8";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(1040, 292);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(41, 12);
+            this.label9.TabIndex = 10;
+            this.label9.Text = "label9";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(14, 562);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(47, 12);
+            this.label10.TabIndex = 11;
+            this.label10.Text = "label10";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(280, 562);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(47, 12);
+            this.label11.TabIndex = 12;
+            this.label11.Text = "label11";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(524, 562);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(47, 12);
+            this.label12.TabIndex = 13;
+            this.label12.Text = "label12";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(785, 562);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(47, 12);
+            this.label13.TabIndex = 14;
+            this.label13.Text = "label13";
+            // 
+            // MainFrm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1286, 835);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lab);
+            this.Controls.Add(this.webBrowser12);
+            this.Controls.Add(this.webBrowser11);
+            this.Controls.Add(this.webBrowser10);
+            this.Controls.Add(this.webBrowser9);
+            this.Controls.Add(this.webBrowser8);
+            this.Controls.Add(this.webBrowser7);
+            this.Controls.Add(this.webBrowser6);
+            this.Controls.Add(this.webBrowser5);
+            this.Controls.Add(this.webBrowser14);
+            this.Controls.Add(this.webBrowser13);
+            this.Controls.Add(this.webBrowser4);
+            this.Controls.Add(this.webBrowser3);
+            this.Controls.Add(this.webBrowser2);
+            this.Controls.Add(this.webBrowser1);
+            this.Name = "MainFrm";
+            this.Text = "网站监测程序";
+            this.Load += new System.EventHandler(this.MainFrm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.WebBrowser webBrowser2;
+        private System.Windows.Forms.WebBrowser webBrowser3;
+        private System.Windows.Forms.WebBrowser webBrowser4;
+        private System.Windows.Forms.WebBrowser webBrowser5;
+        private System.Windows.Forms.WebBrowser webBrowser6;
+        private System.Windows.Forms.WebBrowser webBrowser7;
+        private System.Windows.Forms.WebBrowser webBrowser8;
+        private System.Windows.Forms.WebBrowser webBrowser9;
+        private System.Windows.Forms.WebBrowser webBrowser10;
+        private System.Windows.Forms.WebBrowser webBrowser11;
+        private System.Windows.Forms.WebBrowser webBrowser12;
+        private System.Windows.Forms.WebBrowser webBrowser13;
+        private System.Windows.Forms.WebBrowser webBrowser14;
+        private System.Windows.Forms.Label lab;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WebText
+{
+    public partial class MainFrm : Form
+    {
+        public MainFrm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainFrm_Load(object sender, EventArgs e)
+        {
+            /*孔孟文化遗产*/
+            this.lab.Text = "Ip：114.55.248.19\n\n孔孟文化遗产";
+            this.lab.ForeColor = Color.Red;
+            this.lab.BackColor = Color.Transparent;
+            webBrowser1.Url = new Uri("http://www.sdcmch.com/");
+
+            /*润滑油*/
+            this.label1.Text = "Ip：114.55.248.19\n\n润滑油";
+            this.label1.ForeColor = Color.Red;
+            this.label1.BackColor = Color.Transparent;
+            webBrowser2.Url = new Uri("http://114.55.248.19:9091/yiheyou/login.do");
+
+            /*艾特网络官网*/
+            this.label2.Text = "Ip：115.28.65.110n\n艾特网络官网";
+            this.label2.ForeColor = Color.Red;
+            this.label2.BackColor = Color.Transparent;
+            webBrowser3.Url = new Uri("http://www.willgood.cn/index.html");
+
+            /*保利*/
+            this.label3.Text = "Ip：115.28.65.110\n\n保利";
+            this.label3.ForeColor = Color.Red;
+            this.label3.BackColor = Color.Transparent;
+            webBrowser4.Url = new Uri("http://www.polysdmb.com.cn/");
+
+            /*教育法家协会*/
+            this.label4.Text = "Ip：114.215.116.86\n\n教育法家协会";
+            this.label4.ForeColor = Color.Red;
+            this.label4.BackColor = Color.Transparent;
+            webBrowser5.Url = new Uri("http://www.sdjysx.net/");
+
+            /*方联官网*/
+            this.label5.Text = "Ip：115.28.65.110n\n方联官网";
+            this.label5.ForeColor = Color.Red;
+            this.label5.BackColor = Color.Transparent;
+            webBrowser6.Url = new Uri("http://www.fanglinktec.com/index.html");
+
+
+
+        }
+    }
+}
